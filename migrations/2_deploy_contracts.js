159652e5e40c3b4a74c70111242bf54c2f440fdc
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-03 04:43:28 +0200

    Update migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const Election = artifacts.require("./Election.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Election);
 };
