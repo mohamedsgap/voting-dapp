2396a671c1d64f99a41e10eab37ee1b381753bc6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-22 03:13:57 +0200

    Make app access the user routes

--- backend/app.js
+++ backend/app.js
@@ -15,3 +15,15 @@ mongoose.connect('mongodb+srv://mohamed:un0xlqfbTxUVH7In@cluster0-fytzm.mongodb.
     console.error(error);
 }); 
 
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use(bodyParser.json());
+app.use('/api/auth', userRoutes);
+
+module.exports = app;   
