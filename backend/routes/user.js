0ad3ba44cac6fbb4ca55aa306b8c536444dcc889
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-22 03:25:23 +0200

    Create user signup/login routes

---
+++ backend/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+module.exports = router;
