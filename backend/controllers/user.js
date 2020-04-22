e48d73a4d5cd6bd2f3c5cc30709fb3b37d5f267a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-22 03:19:50 +0200

    Create user login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -25,3 +25,41 @@ exports.signup = (req, res, next) => {
       }
     );
   };
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email }).then(
+      (user) => {
+        if (!user) {
+          return res.status(401).json({
+            error: new Error('User not found!')
+          });
+        }
+        bcrypt.compare(req.body.password, user.password).then(
+          (valid) => {
+            if (!valid) {
+              return res.status(401).json({
+                error: new Error('Incorrect password!')
+              });
+            }
+            const token = jwt.sign({userId: user._id}, 'RANDOM_TOKEN_SECRET', {expiresIn: '24h' });
+            res.status(200).json({
+              userId: user._id,
+              token: token
+            });
+          }
+        ).catch(
+          (error) => {
+            res.status(500).json({
+              error: error
+            });
+          }
+        );
+      }
+    ).catch(
+      (error) => {
+        res.status(500).json({
+          error: error
+        });
+      }
+    );
+  }
