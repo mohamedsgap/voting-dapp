cfb67fe6327d378cbd55dbd92170db844a3e754c
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-06 08:30:10 +0200

    Deploy to Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,21 @@
-const path = require("path");
+const HDWallentProvider = require("truffle-hdwallet-provider");
+require("dotenv").config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  //contracts_build_directory: path.join(__dirname, "client/contracts-deployment"),
   networks: {
     develop: {
       port: 7545
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWallentProvider(
+          process.env.MNEMONIC,
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`
+        ),
+      network_id: 4
     }
   }
 };
