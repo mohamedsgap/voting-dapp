12e79208e96a3f86787499344136e68438602e95
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-14 01:05:11 +0200

    Init AuthProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 //import './App.css';
-import './assets/scss/style.scss';
+import "./assets/scss/style.scss";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain="process.env.DOMAIN"
+    clientId="process.env.CLIENT_ID"
+    redirectUri={window.location.origin}
+  >
+    <Router history={history}>
+      <App />
+    </Router>
+  </Auth0Provider>,
+
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
