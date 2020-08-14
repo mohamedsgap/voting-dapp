a60de81ba81ad68c2bc073d915bdf091319f23b7
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-15 00:09:04 +0200

    update user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
-
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  metaMaskAddress: { type: String, required: true },
+  natId: { type: String, require: true }
 });
 
 userSchema.plugin(uniqueValidator);
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
