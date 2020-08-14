926267ef24703a6f4dfc4366317132597566ab35
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-15 00:24:25 +0200

    setup app connection

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,35 @@
-// MONGODB PW: un0xlqfbTxUVH7In
-// MONGODB CONNECTION: mongodb+srv://mohamed:<password>@cluster0-fytzm.mongodb.net/test?retryWrites=true&w=majority
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+require("dotenv").config();
 
 const app = express();
 
-mongoose.connect('mongodb+srv://mohamed:un0xlqfbTxUVH7In@cluster0-fytzm.mongodb.net/test?retryWrites=true&w=majority').then(() => {
+mongoose
+  .connect(process.env.MONGODB_REAL_CONNECT)
+  .then(() => {
     console.log("'Successfully connected to MongoDB Atlas!'");
-}).catch((error) => {
-    console.log('Unable to connect to MongoDB Atlas!');
+  })
+  .catch(error => {
+    console.log("Unable to connect to MongoDB Atlas!");
     console.error(error);
-}); 
+  });
 
-app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-
 app.use(bodyParser.json());
-app.use('/api/auth', userRoutes);
+app.use("/api/auth", userRoutes);
 
-module.exports = app;   
+module.exports = app;
