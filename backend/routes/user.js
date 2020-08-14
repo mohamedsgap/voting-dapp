2259dba90349edcf2557364f29573990239644d4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-15 00:10:35 +0200

    fix typo in user route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userCtrl = require('../controllers/user');
+const userCtrl = require("../controllers/user");
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post("/sign", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
 module.exports = router;
