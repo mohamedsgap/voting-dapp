6acccee1ba9c903cd20ac14fb7708b77235fbc53
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-08-15 10:11:44 +0200

    fix typo of lib name

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 const User = require("../models/userModel");
